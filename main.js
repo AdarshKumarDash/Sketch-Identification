025561d4271b36c0fe4d286c13e19e1dc4e8c47f
Adarsh Kumar <86519736+AdarshKumarDash@users.noreply.github.com>
2022-04-27 19:35:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,36 @@
 function setup() {
-    canvas = createCanvas(500, 500);
+    canvas = createCanvas(280, 280);
     canvas.center();
+    canvas.mouseReleased(classifyCanvas);
+    synth = window.speechSynthesis;
+}
+
+function preload() {
+    classifier = ml5.imageClassifier("DoodleNet");
+}
+
+function draw() {
+    strokeWeight(12);
+    stroke(0);
+    if (mouseIsPressed) {
+        line(pmouseX, pmouseY, mouseX, mouseY);
+    }
+}
+
+function classifyCanvas() {
+    classifier.classify(canvas, gotResult);
+}
+
+function gotResult(error, result) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(result);
+        document.getElementById("userimg").innerHTML = result[0].label;
+        document.getElementById("confidence").innerHTML = Math.round(result[0].confidence*100) + "%";
+        var utter = new SpeechSynthesisUtterance(result[0].label);
+        synth.speak(utter);
+    }
 }
 
 function clearimg() {
